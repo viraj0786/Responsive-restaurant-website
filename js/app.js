bbf126c6eda45f1e7b4f61e193825f7ddb8451b9
viraj0786 <virajmalviya.pali@gmail.com>
2021-09-17 01:22:48 +0530

    intial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.food-slider').slick({
+       autoplay:true,
+       slidesToShow:3,
+       slidetoscroll:1,
+       prevArrow:".prev-btn",
+       nextArrow:".next-btn"
+    });
+ 
+ });
